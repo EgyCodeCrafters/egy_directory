ff1b8e9828520f8ca5265a5c3f0596d646d1cc6d
Moataz Mohammady <moataz@wuilt.com>
2023-12-20 15:57:49 +0200

    uninstall Voyager

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends \TCG\Voyager\Models\User
+class User extends Authenticatable
 {
     use CrudTrait;
     use HasApiTokens, HasFactory, Notifiable;
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Voyager TODO:: uninstall it !!
-//Route::group(['prefix' => 'admin'], function () {
-//    Voyager::routes();
-//});
-//
 Route::get('/', function (){
     return view('welcome');
 });
