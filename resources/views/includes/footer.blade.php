aeda4112e96719efa12f75f9c75153a6b85d2cc3
Moataz Mohammady <moataz@wuilt.com>
2023-12-25 13:16:10 +0200

    add sendstrap

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -69,6 +69,9 @@
     })()
 
 </script>
+
+<script src="https://app.sendstrap.com/scripts/js/social_button.js?id=4109&key=R2O7D4pfetfOCD6ZvCbddjCgV4MMPuUPhRZapdYy"></script>
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
