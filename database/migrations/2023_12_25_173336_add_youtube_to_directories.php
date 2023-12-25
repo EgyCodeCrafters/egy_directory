3857087912c046be14f4ef1e40741d59b9b4ab38
Moataz Mohammady <moataz@wuilt.com>
2023-12-25 19:35:23 +0200

    add columns to directories table

---
+++ database/migrations/2023_12_25_173336_add_youtube_to_directories.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('directories', function (Blueprint $table) {
+            $table->string('telegram')->nullable();
+            $table->string('youtube')->nullable();
+            $table->string('linkedIn')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('directories', function (Blueprint $table) {
+            $table->dropColumn('telegram');
+            $table->dropColumn('youtube');
+            $table->dropColumn('linkedIn');
+        });
+    }
+};
