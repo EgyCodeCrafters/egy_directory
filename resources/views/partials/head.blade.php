4fcc67709aed29c9e1be8a61607d8fcf728d057b
Moataz Mohammady <moataz.mohammady@gmail.com>
2025-04-10 16:32:16 +0200

    add Bootswatch theme

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -16,6 +16,10 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css"
       integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
+
+<!-- Bootswatch theme -->
+<link href="https://bootswatch.com/5/brite/bootstrap.min.css" rel="stylesheet">
+
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
       integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
 
@@ -23,8 +27,8 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link
-    href="https://fonts.googleapis.com/css2?family=Cairo&family=Karla:ital,wght@1,300&family=Noto+Kufi+Arabic:wght@600&display=swap"
-    rel="stylesheet">
+        href="https://fonts.googleapis.com/css2?family=Cairo&family=Karla:ital,wght@1,300&family=Noto+Kufi+Arabic:wght@600&display=swap"
+        rel="stylesheet">
 
 
 <style>
