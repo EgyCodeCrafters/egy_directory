47864c0fed62048c2be6569f3f403ee06161c9d8
Moataz Mohammady <moataz.mohammady@gmail.com>
2025-04-10 20:42:18 +0200

    enhance footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     </div>
 
 
-    <footer class="row">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
 
         @include('partials.footer')
 
