af52ada355bcdc1254680db556676c9c070898d2
Moataz Mohammady <moataz.mohammady@gmail.com>
2025-04-23 21:48:37 +0200

    * Override the default store action.

--- app/Http/Controllers/Api/DirectoryController.php
+++ app/Http/Controllers/Api/DirectoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\DirectoryRequest;
+use App\Models\CategoryDirectory;
 use App\Models\Directory;
 use LaravelApiBase\Http\Controllers\ApiControllerBehavior;
 
@@ -15,5 +16,33 @@ class DirectoryController extends Controller
     {
         $this->setApiModel($directory);
         $this->setApiFormRequest(DirectoryRequest::class);
+    }
+
+    /**
+     * Override the default store action.
+     */
+    public function store(DirectoryRequest $request)
+    {
+        try {
+            $directory = Directory::create($request->all());
+            $selectedCategories = array_filter($request->input('category_ids'));
+            foreach ($selectedCategories as $category_id) {
+                CategoryDirectory::create([
+                    'category_id' => $category_id,
+                    'directory_id' => $directory->id,
+                ]);
+            }
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'directory created successfully',
+            ], 200);
+        } catch (\Exception $e) {
+            // 4) Catch any unexpected errors
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
     }
 }
