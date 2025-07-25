e7c5d17267ce3cc0f8b552a2cb1df141e30ce61b
Moataz Mohammady <moataz.mohammady@gmail.com>
2025-07-26 00:34:52 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            hmr: false, // this disables hot module reload
         }),
     ],
 });
