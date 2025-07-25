4af1a5cd82bd3c0c2a23548362d9d8d07e512c81
Moataz Mohammady <moataz.mohammady@gmail.com>
2025-07-26 00:51:50 +0300

    build

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('backoffice')
-            ->path(path: 'backoffice')
+            ->path( 'backoffice')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
