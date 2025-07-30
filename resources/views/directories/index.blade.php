d79c96ea5c32d2978646ec6d2eea24a7d70a24bb
Moataz Mohammady <moataz.mohammady@gmail.com>
2025-07-30 19:35:32 +0300

    show only the first 250 characters

--- resources/views/directories/index.blade.php
+++ resources/views/directories/index.blade.php
@@ -1,28 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-
-    @if(!count($directories))
+    @if (!count($directories))
         <div class="alert alert-danger" role="alert">
             التصنيف فازغ
         </div>
-
     @endif
     <div class="row">
         @foreach ($directories as $directory)
             <div class="col-md-4 mb-4">
                 <div class="card bg-light mb-3">
-                    <a href="{{ url('directory',$directory->id) }}">
-                    <div class="card-header">
-                        <h5 class="card-title">  {{ $directory->name }}</h5>
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">{{ $directory->description }}</p>
-                    </div>
+                    <a href="{{ url('directory', $directory->id) }}">
+                        <div class="card-header">
+                            <h5 class="card-title"> {{ $directory->name }}</h5>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">{{ \Illuminate\Support\Str::limit($directory->description, 250) }}</p>
+                        </div>
                     </a>
                 </div>
             </div>
         @endforeach
     </div>
-
 @endsection
